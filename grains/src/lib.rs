256ded537bbc8ba8de6d3348e27b787b659e2cb0
Evan Cameron <evan@vectorface.com>
2017-03-23 17:23:12 -0400

    grains

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,10 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!();
+    if s < 1 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    }
+    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..65).map(|x| square(x)).sum() // 65 not included
 }
