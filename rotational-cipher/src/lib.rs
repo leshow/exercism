34f69987ea907a1de870aa12cd397ae499d06618
Evan Cameron <cameron.evan@gmail.com>
2017-03-25 00:10:27 -0400

    ciphers

---
+++ rotational-cipher/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn rotate(input: &str, shift: u8) -> String {
+    input
+        .chars()
+        .map(|c| {
+            let case = if c.is_uppercase() { b'A' } else { b'a' };
+            if c.is_alphabetic() {
+                ((((c as u8) - case + shift) % 26) + case) as char
+            } else {
+                c
+            }
+        }).collect::<String>()
+}
