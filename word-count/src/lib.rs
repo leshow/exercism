b3b4e1723f79d3a71366f561d7fef3fc0942a6e8
Evan Cameron <cameron.evan@gmail.com>
2017-03-24 20:05:04 -0400

    word-count

---
+++ word-count/src/lib.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+
+pub fn word_count(strings: &str) -> HashMap<String, u32> {
+    let mut map: HashMap<String, u32> = HashMap::new();
+
+    for sub in strings.split(|c: char| !c.is_alphanumeric())
+            .filter(|c| c.len() != 0).map(|c| c.to_lowercase()) {
+        let count = map.entry(sub.to_owned()).or_insert(0);
+        *count += 1;
+    }
+    map
+}
