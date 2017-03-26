e0ff707837017293fbe4e1f2ecb2f63be7eadaa1
Evan Cameron <cameron.evan@gmail.com>
2017-03-25 22:07:09 -0400

    etl

---
+++ etl/src/lib.rs
@@ -0,0 +1,14 @@
+use std::collections::BTreeMap;
+
+pub fn transform(input: &BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
+    // for &(score, ref list) in input.iter() {
+    //     for word in list {
+    //         res.insert(word.to_owned(), score);
+    //     }
+    // }
+    // res
+    // we can do better:
+    input.iter()
+    .flat_map(|(score, list)| list.iter().map(move |word| (word.to_lowercase(), *score)))
+    .collect::<BTreeMap<_, _>>()
+}
