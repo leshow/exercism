7c1bd799c99974ae74f0a9ad3d058fe5bcb3d89a
Evan Cameron <cameron.evan@gmail.com>
2017-04-01 18:29:49 -0400

    roman numerals

---
+++ roman-numerals/src/lib.rs
@@ -0,0 +1,46 @@
+use std::fmt;
+
+const NUMERAL_MAP: [(usize, &'static str); 13] = [(1000, "M"),
+                                                (900, "CM"),
+                                                (500, "D"),
+                                                (400, "CD"),
+                                                (100, "C"),
+                                                (90, "XC"),
+                                                (50, "L"),
+                                                (40, "XL"),
+                                                (10, "X"),
+                                                (9, "IX"),
+                                                (5, "V"),
+                                                (4, "IV"),
+                                                (1, "I")];
+struct Roman {
+    count: usize
+}
+
+impl From<usize> for Roman {
+    fn from(n: usize) -> Self {
+        Roman::new(n)
+    }
+}
+
+impl Roman {
+    pub fn new(n: usize) -> Self {
+        Roman {
+            count: n
+        }
+    }
+}
+
+impl fmt::Display for Roman {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut sum = self.count;
+        let mut res = String::new();
+        for &(val, numeral) in NUMERAL_MAP.iter() {
+            let mut s = sum;
+            while s >=
+            let times = s / val;
+
+        }
+        write!(f, "{}", res);
+    }
+}
