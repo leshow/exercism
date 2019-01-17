55490be334601d128c9b6ba73dbc713ae135069e
Evan Cameron <cameron.evan@gmail.com>
2019-01-17 17:56:03 -0500

    jumping on clouds

---
+++ hackerrank/src/jumping_on_clouds.rs
@@ -0,0 +1,29 @@
+use std::io::{self, BufRead, Write};
+use std::error::Error;
+
+fn jump_clouds(c: &[u8], n: usize) -> usize {
+    let mut jumps = 0;
+    let mut i = 0;
+    while i < c.len() {
+        if i < n - 2 && c[i+2] != 1 {
+            i += 1;
+        }
+        jumps += 1;
+        i += 1;
+    }
+    jumps - 1
+}
+
+fn main() -> Result<(), Box<Error>> {
+    let stdin = io::stdin();
+    let mut stditer = stdin.lock().lines();
+    let n = stditer.next().unwrap()?.parse::<usize>()?;
+    let clouds = stditer.next().unwrap()?;
+    let bin = clouds.split(" ").map(|s| s.parse::<_>()).collect::<Result<Vec<u8>,_>>()?;
+
+    let jumps = jump_clouds(&bin, n);
+    println!("{}", jumps);
+    io::stdout().flush();
+    
+    Ok(())
+}
