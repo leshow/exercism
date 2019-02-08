e61d00a71c429df31f85f70d0d546be9164060bd
Evan Cameron <evan@vectorface.com>
2019-02-08 16:32:02 -0500

    dir reduction

---
+++ hackerrank/src/directionreduction.rs
@@ -0,0 +1,50 @@
+use Direction::*;
+
+#[derive(PartialEq, Clone, Copy)]
+enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+}
+
+fn dir_reduc(arr: &[Direction]) -> Vec<Direction> {
+    arr.iter().fold(Vec::new(), |mut acc, &d| {
+        if acc.is_empty() {
+            acc.push(d);
+            acc
+        } else {
+            let head = acc.last().unwrap();
+            if invert(&head) == d {
+                acc
+            } else {
+                acc.push(d);
+                acc
+            }
+        }
+    })
+}
+
+fn invert(a: &Direction) -> Direction {
+    match a {
+        NORTH => SOUTH,
+        SOUTH => NORTH,
+        EAST => WEST,
+        WEST => EAST,
+    }
+}
+
+fn dir_reduc2(arr: &[Direction]) -> Vec<Direction> {
+    let mut dir = Vec::new();
+    for d in arr {
+        match dir.last() {
+            Some(d2) if invert(&d2) == *d => {
+                dir.pop();
+            }
+            _ => {
+                dir.push(*d);
+            }
+        }
+    }
+    dir
+}
--- hackerrank/src/main.rs
+++ hackerrank/src/main.rs
@@ -2,6 +2,7 @@ mod jumping_on_clouds;
 mod prize_draw;
 mod sock_merchant;
 mod square_digits;
+mod directionreduction;
 
 fn main() {
     println!("Hello, world!");
