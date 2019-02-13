e49b6af2af1fbfc71398e06a6c405ad535533995
Evan Cameron <cameron.evan@gmail.com>
2019-02-12 22:25:51 -0500

    new pythagorean triplet solution

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -4,14 +4,15 @@ use std::collections::HashSet;
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     (1..(sum / 3))
         .into_par_iter()
-        .flat_map(|a| {
-            (a + 1..(sum / 2))
-                .into_par_iter()
-                .filter(move |b| {
-                    let c = sum - a - b;
-                    (a * a) + (b * b) == c * c
-                })
-                .map(move |b| [a, b, (sum - a - b)])
+        .filter_map(|a| {
+            let b_plus_c = sum - a;
+            let b = (b_plus_c.pow(2) - a.pow(2)) / (b_plus_c * 2);
+            let c = b_plus_c - b;
+            if a < b && ((a * a) + (b * b) == c * c) {
+                Some([a, b, c])
+            } else {
+                None
+            }
         })
         .collect::<HashSet<[u32; 3]>>()
 }
