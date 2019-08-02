353f17524d13fb0cb6eb0f9b8fe5c284f8c30604
Evan Cameron <evan@vectorface.com>
2019-08-02 16:15:21 -0400

    leetcode submissions

---
+++ hackerrank/src/delete_columns.rs
@@ -0,0 +1,18 @@
+pub fn min_deletion_size(a: Vec<String>) -> i32 {
+    if a.is_empty() {
+        return 0;
+    }
+    let mut min = 0;
+    let a = a.iter().map(|n| n.as_bytes()).collect::<Vec<_>>();
+    for i in 0..a[0].len() {
+        for j in 1..a.len() {
+            if a[j][i] < a[j - 1][i] {
+                min += 1;
+                break;
+            }
+        }
+    }
+    min
+}
+
+// [1,2] [3,4] [5,6]
--- hackerrank/src/main.rs
+++ hackerrank/src/main.rs
@@ -1,10 +1,12 @@
 #![allow(dead_code)]
 mod acm_icpc_team;
 mod angry_prof;
+mod delete_columns;
 mod different_ways_parentheses;
 mod directionreduction;
 mod jumping_on_clouds;
 mod my_atoi;
+mod number_of_recent_calls;
 mod phantom_test;
 mod prize_draw;
 mod reverse_integer;
