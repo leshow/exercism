047d381a56794338a9ddb588beae58f6ffb0d1db
Evan Cameron <evan@vectorface.com>
2019-11-20 16:52:06 -0500

    Finish N-queens

--- hackerrank/src/n_queens.rs
+++ hackerrank/src/n_queens.rs
@@ -1,3 +1,30 @@
+pub fn solve_n_queens(n: i32) -> Vec<Vec<String>> {
+    let n = n as usize;
+    let mut ret = Vec::new();
+    let mut board = (0..n)
+        .map(|_| (0..n).map(|_| '.').collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    solve(&mut board, n, 0, &mut ret);
+    ret
+}
+
+fn solve(board: &mut Vec<Vec<char>>, n: usize, col: usize, ret: &mut Vec<Vec<String>>) -> bool {
+    if col >= n {
+        let new_board = board.iter().map(|r| r.iter().collect()).collect::<Vec<_>>();
+        ret.push(new_board);
+        return true;
+    }
+    let mut res = false;
+    for row in 0..n {
+        if safe(board, row, col) {
+            board[row][col] = 'Q';
+            res = solve(board, n, col + 1, ret) || res;
+            board[row][col] = '.';
+        }
+    }
+    res
+}
+
 fn safe(board: &[Vec<char>], row: usize, col: usize) -> bool {
     for i in 0..col {
         if board[row][i] == 'Q' {
@@ -24,7 +51,6 @@ fn safe(board: &[Vec<char>], row: usize, col: usize) -> bool {
     }
     true
 }
-
 #[test]
 fn test_safe() {
     let b1 = vec![
