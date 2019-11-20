2f04f4ead17f2ebe3de9d713dda2760617d65355
Evan Cameron <cameron.evan@gmail.com>
2019-11-20 01:09:06 -0500

    add link

--- hackerrank/src/how_many_numbers_iii.rs
+++ hackerrank/src/how_many_numbers_iii.rs
@@ -1,30 +1,19 @@
 // We want to generate all the numbers of three digits where:
-
 //     the sum of their digits is equal to 10.
-
 //     their digits are in increasing order (the numbers may have two or more
 // equal contiguous digits)
-
 // The numbers that fulfill the two above constraints are: 118, 127, 136, 145,
 // 226, 235, 244, 334
-
 // Make a function that receives two arguments:
-
 //     the sum of digits value
-
 //     the desired number of digits for the numbers
-
 // The function should output an array with three values: [1,2,3]
-
 // 1 - the total number of possible numbers
-
 // 2 - the minimum number
-
 // 3 - the maximum number
-
 // The example given above should be:
-
 // findAll 10 3 -> ( 8, Just 118, Just 334 )
+// https://www.codewars.com/kata/how-many-numbers-iii/haskell
 
 use std::cmp;
 
